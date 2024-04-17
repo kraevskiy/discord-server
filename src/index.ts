ef6ab64917a4f5f361052efd186e9c663a5edc11
rambutan <pride.kraievskyi@gmail.com>
2024-04-17 15:12:00 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import cors from "cors";
+import express, { Application } from "express";
+import "dotenv/config";
+import { Server } from "socket.io";
+import {
+	ClientToServerEvents,
+	KeyValue,
+	MeetingType,
+	ServerToClientEvents,
+} from "./types";
+// import { userConnectionEvents } from "./events/user.events";
+
+const PORT = process.env.PORT || 5000;
+const app: Application = express();
+
+app.use(cors());
+
+const expressServer = app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+});
+
+export const meetings: KeyValue<MeetingType> = {};
+
+export const io = new Server<ClientToServerEvents, ServerToClientEvents>(
+	expressServer,
+	{
+		cors: {
+			origin: [process.env.CLIENT_URL || "http://localhost:3000"],
+			methods: ["GET", "POST"],
+		},
+	}
+);
+
+// io.on("connection", userConnectionEvents);
+
+app.get("*", (_, res) => {
+	res.redirect(process.env.CLIENT_URL || "http://localhost:3000");
+});
